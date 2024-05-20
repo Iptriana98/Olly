3b775fdd2ce2e5906466682e741829acdb6ef6d3
ibrahinperez <iptrianaa@gmail.com>
2024-05-20 17:41:29 -0500

    - Updated main Activity

--- app/src/main/java/com/iptriana/olly/MainActivity.kt
+++ app/src/main/java/com/iptriana/olly/MainActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.iptriana.olly.ui.components.OllyTabRow
+import com.iptriana.olly.ui.navigation.OllyNavHost
 import com.iptriana.olly.ui.navigation.OllyTabRowScreens
+import com.iptriana.olly.ui.navigation.Overview
+import com.iptriana.olly.ui.navigation.navigateSingleTopTo
 import com.iptriana.olly.ui.theme.OllyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             OllyApp()
         }
@@ -23,19 +31,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun OllyApp() {
     OllyTheme {
+        val navController = rememberNavController()
+        val currentBackstack by navController.currentBackStackEntryAsState()
+        val currentDestination = currentBackstack?.destination
+        val currentScreen =
+            OllyTabRowScreens.find { it.route == currentDestination?.route } ?: Overview
         Scaffold(
             topBar = {
                 OllyTabRow(
                     allScreens = OllyTabRowScreens,
                     onTabSelected = { screen ->
-//                        navController.navigateSingleTopTo(screen.route)
+                        navController.navigateSingleTopTo(screen.route)
                     },
-                    currentScreen = /*currentScreen*/ null
+                    currentScreen = currentScreen
                 )
             }
         ) { innerPadding ->
-            innerPadding
-//            RallyNavHost(navController = navController, modifier = Modifier.padding(innerPadding))
+            OllyNavHost(navController = navController, modifier = Modifier.padding(innerPadding))
         }
     }
 }
