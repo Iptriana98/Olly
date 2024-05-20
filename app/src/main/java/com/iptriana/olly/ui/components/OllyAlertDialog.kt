f99e327d4a28ff09afef3a32954ce1a56d393dc6
ibrahinperez <iptrianaa@gmail.com>
2024-05-20 17:40:04 -0500

    - Added some utils composable

---
+++ app/src/main/java/com/iptriana/olly/ui/components/OllyAlertDialog.kt
@@ -0,0 +1,46 @@
+package com.iptriana.olly.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import com.iptriana.olly.ui.theme.RallyDialogThemeOverlay
+
+@Composable
+fun OllyAlertDialog(
+    onDismiss: () -> Unit,
+    bodyText: String,
+    buttonText: String
+) {
+    RallyDialogThemeOverlay {
+        AlertDialog(
+            onDismissRequest = onDismiss,
+            text = { Text(bodyText) },
+            confirmButton = {
+                Column {
+                    Divider(
+                        Modifier.padding(horizontal = 12.dp),
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
+                    )
+                    TextButton(
+                        onClick = onDismiss,
+                        shape = RectangleShape,
+                        contentPadding = PaddingValues(16.dp),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(buttonText)
+                    }
+                }
+            }
+        )
+    }
+}
