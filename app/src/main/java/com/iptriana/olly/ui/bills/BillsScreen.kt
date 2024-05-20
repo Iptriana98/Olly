41a375416a538c2f81c499641d100f4ac1905fb1
ibrahinperez <iptrianaa@gmail.com>
2024-05-20 17:40:33 -0500

    - Added application screens

---
+++ app/src/main/java/com/iptriana/olly/ui/bills/BillsScreen.kt
@@ -0,0 +1,30 @@
+package com.iptriana.olly.ui.bills
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import com.iptriana.olly.R
+import com.iptriana.olly.data.Bill
+import com.iptriana.olly.data.UserData
+import com.iptriana.olly.ui.components.BillRow
+import com.iptriana.olly.ui.components.StatementBody
+
+@Composable
+fun BillsScreen(
+    bills: List<Bill> = remember { UserData.bills }
+) {
+    StatementBody(
+        modifier = Modifier.clearAndSetSemantics { contentDescription = "Bills" },
+        items = bills,
+        amounts = { bill -> bill.amount },
+        colors = { bill -> bill.color },
+        amountsTotal = bills.map { bill -> bill.amount }.sum(),
+        circleLabel = stringResource(R.string.due),
+        rows = { bill ->
+            BillRow(bill.name, bill.due, bill.amount, bill.color)
+        }
+    )
+}
